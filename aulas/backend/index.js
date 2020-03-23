f631225937688423c16c17ecaa2f88ae266f6829
xXHachimanXx <globaldep90@gmail.com>
2020-03-23 10:48:38 -0300

    Primeiro dia: configurando ambiente e trabalhando nos primeiros codigos

---
+++ aulas/backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.json({
+        evento: "Semana Omnistack 11.0",
+        aluno: "Felipe (xXHachimanXx) Andrade"
+    });
+});
+
+app.listen(3333);
