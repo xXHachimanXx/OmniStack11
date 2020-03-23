941e18ba79304ab20a42bf6c18b353e383540011
xXHachimanXx <globaldep90@gmail.com>
2020-03-23 11:15:59 -0300

    Primeiro dia: criando primeiros arquivos do backend utilizando o create-react-app

--- aulas/backend/index.js
+++ aulas/backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.json({
         evento: "Semana Omnistack 11.0",
         aluno: "Felipe (xXHachimanXx) Andrade"
