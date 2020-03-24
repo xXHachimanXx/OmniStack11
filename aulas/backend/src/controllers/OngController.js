840ccc95e70e7eeb80e8630b8ab8cb2be4f1bdb9
xXHachimanXx <globaldep90@gmail.com>
2020-03-24 17:17:22 -0300

    Segundo dia: criando controllers para a abstração de requisições das ongs.

---
+++ aulas/backend/src/controllers/OngController.js
@@ -0,0 +1,31 @@
+const crypto = require('crypto');
+const connection = require('../database/connection');
+
+module.exports = {
+    async index(req, res) {
+        const ongs = await connection('ongs').select('*');
+    
+        return res.json(ongs);
+    },
+    
+    async create(req, res) {
+        const { name, email, whatsapp, city, uf } = req.body;
+
+        // Gerar 4 bytes aleatórios, converter para string e retornar em 
+        // forma de hexadecimal
+        const id = crypto.randomBytes(4).toString('HEX');
+
+        // Como o insert pode demorar um pouco, então colocamos um 'await'
+        // para que a execução espere até que a operação seja finalizada.
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        });
+
+        res.json({ id });
+    }
+};
--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -1,37 +1,10 @@
 const express = require('express');
-const crypto = require('crypto');
-const connection = require('./database/connection');
+const OngController = require('./controllers/OngController');
 
 // Desacoplando o módulo de rotas do express em um nova variável
 const routes = express.Router();
-/**
- * Rota / recurso
- */
-routes.get('/ongs', async (req, res) => {
-    const ongs = await connection('ongs').select('*');
 
-    return res.json(ongs);
-});
-
-routes.post('/ongs', async (req, res) => {
-    const { name, email, whatsapp, city, uf } = req.body;
-
-    // Gerar 4 bytes aleatórios, converter para string e retornar em 
-    // forma de hexadecimal
-    const id = crypto.randomBytes(4).toString('HEX');
-
-    // Como o insert pode demorar um pouco, então colocamos um 'await'
-    // para que a execução espere até que a operação seja finalizada.
-    await connection('ongs').insert({
-        id, 
-        name, 
-        email, 
-        whatsapp, 
-        city, 
-        uf
-    });
-    
-    res.json({ id });
-});
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
 module.exports = routes;
