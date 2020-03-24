a6796cc3d8fcccc1e4dde4ea601b32197647130a
xXHachimanXx <globaldep90@gmail.com>
2020-03-24 18:57:14 -0300

    Segundo dia: Criando mais rotas, modificando sistema de paginação para incidents e importando o cors

--- aulas/backend/src/controllers/IncidentControler.js
+++ aulas/backend/src/controllers/IncidentControler.js
@@ -1,11 +1,37 @@
 const connection = require('../database/connection');
 
 module.exports = {
+    
+    /**
+     * Listar todos os incidents utilizando paginação.
+     * 
+     * @param {*} req 
+     * @param {*} res 
+     */
     async index(req, res) {
-        const incidents = await connection('incidents').select('*');
+        const { page = 1 } = req.query;     
+        
+        const [count] = await connection('incidents').count();
+
+        const incidents = await connection('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+            .limit(5) // Limitar em 5 incidents por página
+            .offset( (page - 1) * 5 ) // Pular de 5 em 5 começando de (1-1) * 5 = 0
+            .select([            
+                'incidents.*',
+                'ongs.name',
+                'ongs.email',
+                'ongs.whatsapp',
+                'ongs.city',
+                'ongs.uf'
+            ]);
+
+        // Passar contador de regs para o cabeçalho
+        res.header('X-Total-Count', count['count(*)']);
     
         return res.json(incidents);
     },
+
     async create(req, res) {
         const { title, description, value } = req.body;
 
@@ -19,5 +45,25 @@ module.exports = {
         });
 
         return res.json({ id });        
+    },
+
+    async delete(req, res) {
+        const { id } = req.params;
+        const ong_id = req.headers.authorization;
+
+        const incident = await connection('incidents')
+            .where('id', id)
+            .select('ong_id')
+            .first();
+        
+        if(incident.ong_id !== ong_id) {
+            // conferir mais sobre status de requisição na documentação
+            return res.status(401).json({ error: "Operation not permitted."});
+        }
+
+        // Deletar registro 
+        await connection('incidents').where('id', id).delete();
+
+        return res.status(204).send(); // Status vazio
     },
 };
--- aulas/backend/src/index.js
+++ aulas/backend/src/index.js
@@ -1,10 +1,13 @@
 const express = require('express'); // não usa-se ./ pois este não é um arquivo
+const cors = require('cors');
 const routes = require('./routes'); // usa-se ./ pois este é um arquivo
+
 
 const app = express();
 
 // Isso vai dizer ao express que o body da requisição vai 
 // ser entendida com um JSON.
+app.use(cors());
 app.use(express.json());
 app.use(routes); // Utilizar rotas
 
