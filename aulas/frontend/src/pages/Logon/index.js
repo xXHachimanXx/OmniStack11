8438b33b1f9433587ab03d63ae7eb546e1cfe025
xXHachimanXx <globaldep90@gmail.com>
2020-03-26 14:44:25 -0300

    Terceiro dia: Finalizando o frontend da página.

--- aulas/frontend/src/pages/Logon/index.js
+++ aulas/frontend/src/pages/Logon/index.js
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiLogIn } from 'react-icons/fi';
-import { Link } from 'react-router-dom'; // mudar a rota sem recarregar a página
+import { Link, useHistory } from 'react-router-dom'; // mudar a rota sem recarregar a página
 
 import './styles.css';
 
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
+import api from '../../services/api';
+
 export default function Logon() {
+
+    const [id, setId] = useState('');
+
+    const history = useHistory();
+
+    async function hundleLogin(e) {
+        e.preventDefault();
+
+        try {
+          const res = await api.post('sessions', {id});  
+
+          localStorage.setItem("ongId", id);
+          localStorage.setItem("ongName", res.data.name);
+
+          history.push('/profile');
+
+        } catch (error) {
+            alert('Falha no login. Tente novamente.');
+        }
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be The Hero" />
 
-                <form>
+                <form onSubmit={hundleLogin}>
                     <h1>Faça seu logon</h1>
 
-                    <input placeholder="Sua ID" />
+                    <input 
+                        placeholder="Sua ID" 
+                        value={id}
+                        onChange={ e =>  setId(e.target.value)}
+                    />
                     <button className="button" type="submit">Entrar</button>
 
                     <Link className="back-link" to="/register">
--- aulas/frontend/src/pages/Profile/index.js
+++ aulas/frontend/src/pages/Profile/index.js
@@ -1,83 +1,90 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import logoImg from '../../assets/logo.svg';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 export default function Profile() {
+
+    // Como é um conjuto de dados então 
+    // começaremos com o array vazio
+    const [incidents, setIncidents] = useState([]);
+
+    const history = useHistory();
+
+
+    // Pegar da armazenamento local do navegador
+    // dca0fe2a
+    const ongName = localStorage.getItem('ongName');
+    const ongId = localStorage.getItem('ongId');
+
+
+    // Toda vez que alguma var do arranjo(passado no 
+    // parâmetro)for alterada, ele executa a função passada 
+    // no parâmetro
+    useEffect( () => {
+        api.get('profile', {
+            headers: { Authorization: ongId }
+        }).then( res => {
+            setIncidents(res.data);
+        });
+    }, [ongId]); // Passamos [ongId] somente por convenção
+
+    async function hundleDeleteIncident(id) {
+        try {
+            await api.delete(`incidents/${id}`, {
+                headers: { Authorization: ongId }
+            });
+
+            await setIncidents(incidents.filter( incident => incident.id !== id ));
+
+        } catch (error) {
+            alert("Erro ao deletar o incidente. Tente novamente.");
+        }
+    }
+
+    async function hundleLogout() {
+        localStorage.clear();
+
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be The Hero" />
-                <span>Bem vinda a APAD</span>
+                <span>Bem vinda a {ongName}</span>
 
                 <Link className="button" to="/incidents/new"> Cadastrar novo caso</Link>
-                <button type="button"> 
+                <button onClick={hundleLogout} type="button"> 
                     <FiPower size={18} color="#e02041"/>
                 </button>
             </header>
 
             <h1>Casos cadastrados</h1>
 
-            <ul>
-                <li>
+            <ul>                
+                { incidents.map( incident => (
+                    <li key={ incident.id }>
                     <strong>CASO:</strong>
-                    <p>Caso teste</p>
+                    <p>{ incident.title }</p>
 
                     <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
+                    <p>{ incident.description }</p>
 
                     <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button type="button">
+                    <p>{ 
+                        Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value) 
+                    }</p>                    
+                    <button onClick={ () => { hundleDeleteIncident(incident.id) } } type="button">
                         <FiTrash2 size={20} color="#a8a8b3"/>
                     </button>
                 </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$ 120,00</p>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3"/>
-                    </button>
-                </li>
+                ))}
             </ul>
         </div>
     );
